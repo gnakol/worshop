f8622bfb247d4845b9251d6ee7142e39b04ad7ae
Ngna Kolie <gna.kolie@yahoo.fr>
2022-10-11 11:43:14 +0200

    Project initialiser

---
+++ src/test/java/fr/epsi/groupe4/worshop/worshop/WorshopApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.groupe4.worshop.worshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
