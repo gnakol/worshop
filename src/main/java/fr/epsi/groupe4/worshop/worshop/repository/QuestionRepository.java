f2c640c9947347ae3f63fa68e762b7bc9bba8919
Ngna Kolie <gna.kolie@yahoo.fr>
2022-10-12 09:39:07 +0200

    mise à jours de question service

--- src/main/java/fr/epsi/groupe4/worshop/worshop/repository/QuestionRepository.java
+++ src/main/java/fr/epsi/groupe4/worshop/worshop/repository/QuestionRepository.java
@@ -1,9 +1,10 @@
 package fr.epsi.groupe4.worshop.worshop.repository;
 
+import fr.epsi.groupe4.worshop.worshop.beans.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuestionRepository extends JpaRepository<QuestionRepository, Integer> {
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
 }
--- src/main/java/fr/epsi/groupe4/worshop/worshop/services/QuestionService.java
+++ src/main/java/fr/epsi/groupe4/worshop/worshop/services/QuestionService.java
@@ -1,7 +1,44 @@
 package fr.epsi.groupe4.worshop.worshop.services;
 
+import fr.epsi.groupe4.worshop.worshop.beans.Question;
+import fr.epsi.groupe4.worshop.worshop.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    public List<Question> listeQuestion()
+    {
+        return this.questionRepository.findAll();
+    }
+
+    public void ajouterQuestion(Question question)
+    {
+        questionRepository.save(question);
+    }
+
+    public Question updateQuestion(int id_question, Question question)
+    {
+        return questionRepository.findById(id_question)
+                .map(p->{
+                    p.setDate(question.getDate());
+                    p.setReponse(question.getReponse());
+                    return questionRepository.save(p);
+    }).orElseThrow(()-> new RuntimeException("desoler question non trouver"));
+    }
+
+    public void removeQuestion(int id_question)
+    {
+        
+    }
 }
